3beef93feddbc196afb87ecad50b92bcdeb408d8
Sabus123 <msm01777946259@gmail.com>
2025-01-31 00:13:41 +0600

    Done features part design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-  /** @type {import('tailwindcss').Config} */
-  module.exports = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
